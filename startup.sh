8a100ff291cce51155338d70ec13766b585b6d3c
keithvsr <keithvsr@gmail.com>
2020-04-03 12:08:27 -0700

    adds repo link to index and some startup.sh reformatting

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-node=`uname -n`
+# basic bash to pre-setup nginx when deployed on heroku etc
+# make sure line endings are LF only
+node=`uname -n` # get current hostname (heroku hostnames are long)
 length=${#node}
 if [ $length -gt 16 ]
     then
@@ -10,5 +12,6 @@ if [ $length -gt 16 ]
         echo '[#####] short nodename - assuming not heroku'
 fi
 echo '[#####] starting nginx'
+
 # last up, start nginx
 exec nginx -g "daemon off;"
